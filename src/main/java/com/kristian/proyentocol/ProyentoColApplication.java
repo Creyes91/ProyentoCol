438916c8d4f7af7dd8bd6eaa7f957c97379ceb2f
Cristian <cd.reyes@icloud.com>
2026-07-14 19:24:16 +0200

    Initial commit

---
+++ src/main/java/com/kristian/proyentocol/ProyentoColApplication.java
@@ -0,0 +1,13 @@
+package com.kristian.proyentocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyentoColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyentoColApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kristian/proyentocol/ProyentoColApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristian.proyentocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyentoColApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
