ce66861686821a4c6d184ba0edb31330f9191be3
Cristian <cd.reyes@icloud.com>
2026-07-14 19:24:35 +0200

    format: update pom.xml and Java files for consistent indentation

--- src/main/java/com/kristian/proyentocol/ProyentoColApplication.java
+++ src/main/java/com/kristian/proyentocol/ProyentoColApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyentoColApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyentoColApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProyentoColApplication.class, args);
+    }
 
 }
